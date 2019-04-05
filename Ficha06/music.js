f08e0722479e93c944f9fc578fd9b64f62f2d439
diana <9180189@esmad.ipp.pt>
2019-04-05 16:31:16 +0100

    music ficha

---
+++ Ficha06/music.js
@@ -0,0 +1,17 @@
+class Band{
+constructor(name, genre, creationDate, origin, nMembers, photo, wikipedia){
+    this.name = name 
+    this.genre = genre
+    this.creationDate = creationDate
+    this.origin = origin
+    this.nMembers = nMembers
+    this.photo = photo
+    this.wikipedia = wikipedia
+
+    }
+}
+
+const myBand = new Band("Imagine Dragons", "Pop/Rock", 2008, 4,"https://pl.scdn.co/images/pl/default/58a222e0c76ce86ed68121f5216b838b0ab8f338", "https://pt.wikipedia.org/wiki/Imagine_Dragons")
+const myBand2 = new Band("Muse","Pop/Rock", 1994, 4, "http://www.planckmachine.com/wp-content/uploads/2016/09/hysteria-muse-meaning-song.jpg", "https://pt.wikipedia.org/wiki/Muse")
+
+
